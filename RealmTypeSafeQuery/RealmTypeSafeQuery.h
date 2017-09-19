9d5646fd1a9d48307f4a7c1da0aceca6b864018f
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2017-09-20 01:42:58 +0900

    Initial Commit

---
+++ RealmTypeSafeQuery/RealmTypeSafeQuery.h
@@ -0,0 +1,19 @@
+//
+//  RealmTypeSafeQuery.h
+//  RealmTypeSafeQuery
+//
+//  Created by Kishikawa Katsumi on 2017/09/20.
+//  Copyright © 2017 Kishikawa Katsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmTypeSafeQuery.
+FOUNDATION_EXPORT double RealmTypeSafeQueryVersionNumber;
+
+//! Project version string for RealmTypeSafeQuery.
+FOUNDATION_EXPORT const unsigned char RealmTypeSafeQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmTypeSafeQuery/PublicHeader.h>
+
+
---
+++ RealmTypeSafeQueryTests/RealmTypeSafeQueryTests.swift
@@ -0,0 +1,36 @@
+//
+//  RealmTypeSafeQueryTests.swift
+//  RealmTypeSafeQueryTests
+//
+//  Created by Kishikawa Katsumi on 2017/09/20.
+//  Copyright © 2017 Kishikawa Katsumi. All rights reserved.
+//
+
+import XCTest
+@testable import RealmTypeSafeQuery
+
+class RealmTypeSafeQueryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
